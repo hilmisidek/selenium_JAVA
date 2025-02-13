3a6337a93b7b13df042954347c58922d5ddbe5de
hilmisidek <khairulhilmi.sidek@petronas.com>
2025-02-13 17:47:46 +0800

    add

--- src/App.java
+++ src/App.java
@@ -1,19 +1,23 @@
 import static org.junit.Assert.assertEquals;
 
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 //import org.apache.poi;
 
 
-
+@FixMethodOrder( MethodSorters.NAME_ASCENDING )
 public class App {
     
     
@@ -27,13 +31,13 @@ public class App {
     }
         
     @Test
-    public void test_url(){
+    public void test_1_url(){
         driver.get("https://qa-practice.netlify.app/auth_ecommerce");
       
     }
 
     @Test
-    public void login(){
+    public void test_2_login(){
        
         loginPage dologin = new loginPage(driver);
         dologin.login("admin@admin.com", "admin123");
@@ -44,19 +48,22 @@ public class App {
     }
 
     @Test
-    public void check_price(){
+    public void test_3_check_price(){
         newshop.check_price();
         
     }
 
     @Test
-    public void add_toCart(){
+    public void test_4_add_toCart(){
         newshop.add_toCart();
     }
 
-    
+    @Test
+    public void test_5_add_tocart_quantity(){
+        newshop.add_quantity(3);
+    }
 
-
+   
 
 
     /*public static void main(String[] args) throws Exception {
--- src/shop.java
+++ src/shop.java
@@ -1,6 +1,16 @@
+import java.text.DecimalFormat;
+import java.time.Duration;
+
+
 import org.junit.Assert;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v130.page.model.WebAppManifest;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class shop {
 
@@ -10,6 +20,11 @@ public class shop {
     By btn_addCart;
     By added_cart_item;
     By added_cart_price;
+    By cart_quantity;
+    By cart_remove;
+    By cart_total;
+    DecimalFormat df;
+    
 
     Double ex_price;
 
@@ -21,7 +36,12 @@ public class shop {
         this.btn_addCart = By.xpath("//span[@class='shop-item-title'][text()='" + test_product + "']/following-sibling::div/button[text()='ADD TO CART']");
         this.added_cart_item = By.xpath("//div[@class='cart-items']//div[@class='cart-item cart-column']/span[text()='" + test_product +"']");
         this.added_cart_price = By.xpath("//div[@class='cart-items']//div[@class='cart-item cart-column']/span[text()='"+ test_product + "']/parent::div/following-sibling::span[@class='cart-price cart-column']");
-        
+        //this.cart_quantity = By.xpath("//div[@class='cart-items']//div[@class='cart-item cart-column']/span[text()='" + test_product + "']/parent::div/following-sibling::div[@class='cart-quantity cart-column']/input[@type='number']");
+        this.cart_quantity = By.cssSelector("input.cart-quantity-input");
+        this.cart_remove = By.xpath("//div[@class='cart-items']//div[@class='cart-item cart-column']/span[text()='" + test_product + "']/parent::div/following-sibling::div[@class='cart-quantity cart-column']/button[text()='REMOVE']");
+        this.cart_total = By.cssSelector(".cart-total-price");
+        this.df = new DecimalFormat("0.00");
+
 
     }
 
@@ -38,16 +58,41 @@ public class shop {
         driver.findElement(btn_addCart).click();
         Assert.assertTrue(driver.findElement(added_cart_item).isDisplayed());
         String add_cart_priceString = driver.findElement(added_cart_price).getText();
-        String add_cart_moneyString = add_cart_priceString.substring(1,add_cart_priceString.length());
-        Double add_cart_lastPrice = Double.parseDouble(add_cart_moneyString);
+        Double add_cart_lastPrice = price_fetch(add_cart_priceString);
         Assert.assertEquals(ex_price, add_cart_lastPrice);
 
     }
 
-    public void add_quantity(){
+    public void add_quantity(Integer quantity){
+        String send_quantity= Integer.toString(quantity);
+        //WebElement cart_quantity_element = driver.findElement(cart_quantity);
+        new WebDriverWait(driver,Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfElementLocated(cart_quantity));
+        String get_quantity= driver.findElement(cart_quantity).getDomAttribute("value");
+        System.out.println("QUANTITY READ=" + get_quantity);
+        driver.findElement(cart_quantity).sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        driver.findElement(cart_quantity).sendKeys(send_quantity);
+        driver.findElement(cart_total).click();
 
+        Double price_check = price_fetch(driver.findElement(added_cart_price).getText());
+        Double exp_cart_total = Double.parseDouble(df.format(quantity * price_check));
         
+        Double cart_total_string = price_fetch(driver.findElement(cart_total).getText());
+        Assert.assertEquals(exp_cart_total,cart_total_string);
+        //Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        
+        
+        
+       
     }
 
+    public Double price_fetch(String price_raw){
+        String price_text = price_raw.substring(1,price_raw.length());
+        Double last_price = Double.parseDouble(price_text);
+        return last_price;
+
+
+    }
+
+   
     
 }
